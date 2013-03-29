4eac448213638d3705fa835104e4d36874d95e6b
Erika Johnson <erikajoh@usc.edu>
2013-03-29 15:27:32 -0700

    PuzzleMove Class Implementation, close #4

--- puzzle_move.cpp
+++ puzzle_move.cpp
@@ -1,10 +1,37 @@
 #include "puzzle_move.h"
 
+PuzzleMove::PuzzleMove(Board &b)
+{
+  g_ = 0;
+  prev_ = NULL;
+  ManhattanHeuristic score;
+  h_ = score.compute(b.getTiles(),b.getSize());
+}
 
+PuzzleMove::PuzzleMove(int tile, Board *b, PuzzleMove *parent)
+{
+  g_=0;
+  tileMove_ = tile;
+  ManhattanHeuristic score;
+  Board tempb(*b);
+  tempb.move(tile);
+  b_ = &tempb;
+  h_ = score.compute(b_->getTiles(),b_->getSize());
 
+  prev_ = parent;
+  PuzzleMove *temp = prev_;
+  while(prev_){
+    g_++;
+    temp = temp->prev_;
+  }
+}
 
+PuzzleMove::~PuzzleMove()
+{
 
-// Compare to PuzzleMoves based on f distance (needed for priority queue
+}
+
+// Compare to PuzzleMoves based on f distance (needed for priority queue)
 bool PuzzleMove::operator<(const PuzzleMove& p) const
 {
   if((g_ + h_) < (p.g_ + p.h_)){
@@ -17,6 +44,7 @@ bool PuzzleMove::operator<(const PuzzleMove& p) const
     return false;
   }
 }
+
 bool PuzzleMove::operator>(const PuzzleMove& p) const
 {
   if( (g_ + h_) > (p.g_ + p.h_) ){
@@ -28,6 +56,13 @@ bool PuzzleMove::operator>(const PuzzleMove& p) const
   else {
      return false;
   }
-  
 }
 
+bool PuzzleMove::operator==(const PuzzleMove& p) const
+{
+  if( (g_ + h_) == (p.g_ + p.h_) ){
+    return true;
+  } else {
+     return false;
+  }
+}
